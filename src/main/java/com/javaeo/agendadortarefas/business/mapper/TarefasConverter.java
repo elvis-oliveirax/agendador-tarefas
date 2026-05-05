cd872a6faf16a7838b3a62f376e5a767cba0a5fa
Elvis <elvisoliveira0100@hotmail.com>
2026-05-05 16:02:43 -0300

    DTO para record e corrigindo mappers

--- src/main/java/com/javaeo/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/javaeo/agendadortarefas/business/mapper/TarefasConverter.java
@@ -1,6 +1,6 @@
 package com.javaeo.agendadortarefas.business.mapper;
 
-import com.javaeo.agendadortarefas.business.dto.TarefasDTO;
+import com.javaeo.agendadortarefas.business.dto.TarefasDTORecord;
 import com.javaeo.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,11 +14,11 @@ public interface TarefasConverter {
 	@Mapping(source = "dataEvento", target = "dataEvento")
 	@Mapping(source = "dataCriacao", target = "dataCriacao")
 
-	TarefasEntity paraTarefaEntity(TarefasDTO dto);
+	TarefasEntity paraTarefaEntity(TarefasDTORecord dto);
 
-	TarefasDTO paraTarefasDTO(TarefasEntity entity);
+	TarefasDTORecord paraTarefasDTORecord(TarefasEntity entity);
 
- 	List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+ 	List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTORecord> dtos);
 
-	List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
+	List<TarefasDTORecord> paraListaTarefasDTORecord(List<TarefasEntity> entities);
 }
